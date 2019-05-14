1cb521822f6096c8b20f67e50fa95db4356c52ad
Golik Roman <romangolik86786@gmail.com>
2019-05-14 06:13:23 +0300

    CourseWork

---
+++ Програма/Файли програми/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Виберіть файл\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.openFile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Зашифрувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.saveEncryptedFile);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Розшифрувати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saveDecryptedFile);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Виберіть файл котрий бажаєте зашифрувати/розшифрувати";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Оберіть потрібну дію";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(136, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 25);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Інструкція";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Instruction);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(524, 31);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 23);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(509, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ключ шифрування";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(437, 109);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 25);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Зашифрований";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.openAnEncryptedFile);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(437, 140);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 25);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Розшифрований";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.openDecryptedFile);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(373, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(348, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Переглянути зашифрований/розшифрований файл";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(595, 254);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 25);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Вихід";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.closeTheProgram);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(245, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Інструкція користування програмою";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 303);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Шифр Цезаря";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Програма/Файли програми/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string fileName; //змінна, яка запам'ятовує шлях та ім'я обраного для шифрування/дешифрування файлу
+        private string saveEncrypted; //змінна, запам'ятовує шлях та ім'я збереженого зашифрованого файлу
+        private string saveDecoded; //змінна, запам'ятовує шлях та ім'я збереженого дешифрованого файлу
+        private char[] textArray; //масив символів, для того щоб працювати з текстом посимвольно
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile(object sender, EventArgs e) //кнопка, при натисненні на яку повинно з'явитися діалогове вікно для вибору файлу
+        {
+            openFileDialog1.Filter = "(*.txt) | *.txt"; //задаємо обмеження, щоб вибиралися файли тільки з таким розширенням
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //якщо, після закриття вікна, результат повернення дорівнює OK, то ім’я вибраного файлу зберігається у властивості FileName.
+            {
+                fileName = openFileDialog1.FileName; //запам'ятовуємо шлях та ім'я файлу в змінній fileName
+            }
+        }
+
+        private void saveEncryptedFile(object sender, EventArgs e) //функція для збереження зашифрованого файлу
+        {
+            if (fileName == null) //перевіряємо чи користувач обрав файл для шифрування
+            {
+                MessageBox.Show("Оберіть файл котрий хочете зашифрувати.");
+            }
+            else
+            {
+                saveFileDialog1.Filter = "(*.txt) | *.txt"; //задаємо обмеження, щоб зберігалися файли тільки з текстовим розширенням
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK) //якщо, після закриття вікна, результат повернення дорівнює OK, то ім’я вибраного файлу зберігається у властивості FileName.
+                {
+                    saveEncrypted = saveFileDialog1.FileName; //запам'ятовуємо шлях та ім'я файлу в змінній saveEncrypted
+                    MessageBox.Show("Файл зашифровано"); //повідомляємо про те, що операція пройшла успішно
+                }
+                encryptionCaesar(); //функція, що виконує шифрування тексту
+            }
+        }
+
+        private void encryptionCaesar() //функція шифрування тексту
+        {
+            string skey = numericUpDown1.Value.ToString(); //отримуємо значення ключа шифрування
+            int key = Convert.ToInt32(skey); //перетворюємо отримане значення в числовий тип
+            string text = "";
+            StreamReader sr = new StreamReader(fileName, Encoding.Default); //потік для читання тфайлу
+            while (!sr.EndOfStream) //читається весь текст до кінця
+            {
+                text += sr.ReadLine();
+            } 
+            sr.Close(); //закриваємо потік читання
+            StreamWriter sw = new StreamWriter(saveEncrypted); //потік, для запису у зашифрованого тексту в новий файл
+            textArray = text.ToCharArray(); //перетворюємо наш текст у масив символів
+            for (int i = 0; i < textArray.Length; i++) //проходимя по усим символам
+            {
+                if (textArray[i] >= 'a' && textArray[i] <= 'z') //перевіряємо чи символ належить множині a-z
+                {
+                    char encryptedText = (char)((textArray[i] - 'a' + key) % 26 + 'a'); //зміщуємо символ на наш ключ
+                    sw.Write(encryptedText); //записуємо посимвольно наший замінений символ у файл
+                }
+                else if (textArray[i] >= 'A' && textArray[i] <= 'Z') //перевіряємо чи символ належить множині A-Z
+                {
+                    char encryptedText = (char)((textArray[i] - 'A' + key) % 26 + 'A'); //также само
+                    sw.Write(encryptedText);
+                }
+                else
+                {
+                    sw.Write(textArray[i]); //якщо наш символ не буква, то записуємо його як є
+                }
+            }
+            sw.Close(); // закриваємо наш потік
+        }
+        
+        private void saveDecryptedFile(object sender, EventArgs e) //функція для збереження розшифрованого файлу, поясненя такі ж
+        {
+            if (fileName == null) 
+            {
+                MessageBox.Show("Оберіть файл котрий хочете розшифрувати."); 
+            }
+            else
+            {
+                saveFileDialog1.Filter = "(*.txt) | *.txt"; 
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
+                {
+                    saveDecoded = saveFileDialog1.FileName; 
+                    MessageBox.Show("Файл розшифровано"); 
+                }
+                decryptionOfCaesar(); //функція, що виконує дешифровку тексту
+            }
+        }
+
+        private void decryptionOfCaesar() //функція дешифрування тексту, такий же алгоритм як і при шифруванні
+        {
+            string skey = numericUpDown1.Value.ToString();
+            int key = Convert.ToInt32(skey);
+            string text = "";
+            StreamReader sr = new StreamReader(fileName, Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                text += sr.ReadLine();
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter(saveDecoded);
+            textArray = text.ToCharArray();
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                if (textArray[i] >= 'a' && textArray[i] <= 'z')
+                {
+                    char decodedText = (char)((textArray[i] - 'z' - key) % 26 + 'z');
+                    sw.Write(decodedText);
+                }
+                else if (textArray[i] >= 'A' && textArray[i] <= 'Z')
+                {
+                    char decodedText = (char)((textArray[i] - 'Z' - key) % 26 + 'Z');
+                    sw.Write(decodedText);
+                }
+                else
+                {
+                    sw.Write(textArray[i]);
+                }
+            }
+            sw.Close();
+        }
+        
+        private void Instruction(object sender, EventArgs e) //кнопка інструкції до програми
+        {
+            MessageBox.Show("Програма для шифрування і дешифрування тексту англійської мови шифром Цезаря.\nПринцип дії полягає в тому, щоб циклічно зсунути алфавіт, а ключ — це кількість літер, на які робиться зсув.\n" +
+                "       1.Спочатку ви повинні обрати файл котрий бажаєте зашифрувати або дешифрувати.\n" +
+                "       2.Потім задайте значення ключа шифрування.\n" + 
+                "       3.Оберіть потрібну вам операцію.\n" +
+                "       4.По бажанню можете переглянути вміст файлу який був зашифрований або розшифрований.\n" +
+                "Вдалого вам користування!!!", "Інструкція");
+            
+        }
+
+        private void openAnEncryptedFile(object sender, EventArgs e) //перегляд зашифрованого тексту
+        {
+            try //пробуємо відкрити файл 
+            {
+                System.Diagnostics.Process.Start(saveEncrypted); //відкриваємо наш файл
+            }
+            catch(Exception exp) //якщо файлу неіснує, виводимо повідомлення про це
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void openDecryptedFile(object sender, EventArgs e) //перегляд розшифрованого тексту, такий же алгоритм
+        {
+            try 
+            {
+                System.Diagnostics.Process.Start(saveDecoded);
+            }
+            catch (Exception exp) 
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void closeTheProgram(object sender, EventArgs e) //закриття програми
+        {
+            Application.Exit(); //виконує власне закриття нашої програми
+        }
+    }
+}
